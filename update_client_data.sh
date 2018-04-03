4771ac7b5a02e61001fe7a1efc66b37d8170fdea
Niklas Carlsson <niklas.carlsson1994@gmail.com>
2018-04-03 22:05:15 +0200

    Clean client data when syncing

--- update_client_data.sh
+++ update_client_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # fetch client data from client git
-git clone https://github.com/ElitCoder/Kobla-2D-Client.git
+git clone https://github.com/ElitCoder/Kobla-2D-Client.git && rm -rf bin/client_data
 mkdir -p bin/client_data/
 cp -r Kobla-2D-Client/bin/data/* bin/client_data
 
