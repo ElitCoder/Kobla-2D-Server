d368b7cbd7ffcf77aeedc5af88863efc5ca7bc09
Niklas <niklas.carlsson1994@gmail.com>
2018-05-09 00:44:13 +0200

    Always remove Objects when hitting something

--- src/Map.cpp
+++ src/Map.cpp
@@ -77,11 +77,13 @@ void Map::react() {
 	
 	for (auto& object : objects_) {
 		if (!object.move()) {
+			// Tell the Clients that this Object is gone
+			Base::game().removeObject(&object);
+			
 			// We hit something
 			auto& information = object.getCollisionInformation();
 			
 			if (information.getType() == COLLISION_MONSTERS) {
-				Base::game().removeObject(&object);
 				Base::game().removeMonster(information.getID());
 			}
 			
